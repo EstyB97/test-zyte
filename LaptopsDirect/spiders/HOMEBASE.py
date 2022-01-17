23beac2da222724bb77928706b7143cffa2f5e60
EstyB97 <BrandonEsteves@buyitdirect.co.uk>
2022-01-17 22:19:14 +0000

    homebase

--- LaptopsDirect/spiders/HOMEBASE.py
+++ LaptopsDirect/spiders/HOMEBASE.py
@@ -5,11 +5,31 @@ from LaptopsDirect.items import LaptopsdirectItem
 #from scrapy.linkextractors import LinkExtractor
 import socket
 
-class ORIONACSpider(SitemapSpider):
-    name = 'ORIONAC'
+class HOMEBASESpider(SitemapSpider):
+    name = 'HOMEBASE'
+    custom_settings = { 
+      'DOWNLOAD_DELAY': '0.08', 
+      'AUTOTHROTTLE_START_DELAY': '4',
+      'AUTOTHROTTLE_MAX_DELAY': '80', 
+      'AUTOTHROTTLE_TARGET_CONCURRENCY': '16', 
+      'CLOSESPIDER_ERRORCOUNT': '500',
+      'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362',
+      'CONCURRENT_REQUESTS': '24', 
+      'ROBOTSTXT_OBEY': 'False',
+   }
+   #custom_settings = { 
+        #'DOWNLOADER_MIDDLEWARES' : {
+            #'LaptopsDirect.middlewares.CustomProxyMiddleware': 350,
+            #'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 400,
+            #'LaptopsDirect.middlewares.RotatingProxyMiddleware': 610,
+            #'LaptopsDirect.middlewares.BanDetectionMiddleware': 620,
+        #},
+        #'CLOSESPIDER_TIMEOUT' : '864000'
+    #}
+
 
     allowed_domains = ['homebase.co.uk']
-    sitemap_urls = ['https://www.homebase.co.uk/sitemapindex-product.xml.gz']
+    sitemap_urls = ['https://www.homebase.co.uk/sitemap-product-0.xml.gz']
 
 
     def parse(self, response):
@@ -18,7 +38,7 @@ class ORIONACSpider(SitemapSpider):
         BB.add_xpath ('title','//h1[@class="productName_title"]/text()')
         BB.add_xpath ('sku','//div[@id="product-description-content-lg-9"]//div[@data-information-component="hbg_modelNumber"]/div/text()')
         BB.add_xpath ('sku','//div[@class="externalSku"]/text()')
-        BB.add_xpath ('price','//p[@class="productPrice_price "]/text()')
+        BB.add_xpath ('price','//p[@data-product-price="price"]/text()', re=r"(.+)")
         BB.add_xpath ('stock','//p[@class="productStockInformation_prefix"]/text()')
 
         BB.add_value('url', response.url)
@@ -29,3 +49,5 @@ class ORIONACSpider(SitemapSpider):
         BB.add_value('date', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
         return BB.load_item()
+
+        
